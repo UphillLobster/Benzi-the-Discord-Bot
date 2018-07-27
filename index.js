ade41d75ff0bbb9d0a495a4770b0fb9412bc126d
armedturret <rjwhitenh@outlook.com>
2018-07-27 16:50:16 -0400

    Made commands use arguments

--- index.js
+++ index.js
@@ -7,17 +7,6 @@ myfs.check();
 
 const PREFIX = myfs.getConfig().prefix;
 
-CLIENT.on('ready', () => {
-  console.log(`Logged in as ${CLIENT.user.tag}!`);
-});
-
-CLIENT.on('message', async msg => {
-  if (!msg.content.startsWith(PREFIX))
-    return;
-  if(msg.content.startsWith(PREFIX + 'ping'))
-    await msg.channel.send(`Latency is ${CLIENT.ping}ms`);
-});
-
 function makeArgs(message) {
 /*  var arr = message.split(' ');
   for (var word = 0; word <= arr.length - 1; word++) {
@@ -26,5 +15,18 @@ function makeArgs(message) {
   return message.split(' ');
 }
 
+CLIENT.on('ready', () => {
+  console.log(`Logged in as ${CLIENT.user.tag}!`);
+});
+
+CLIENT.on('message', async msg => {
+  if (!msg.content.startsWith(PREFIX))
+    return;
+
+  const msgargs = makeArgs(msg.content);
+
+  if(msgargs[0] === PREFIX + 'ping')
+    await msg.channel.send(`Pong! Latency is ${CLIENT.ping}ms.`);
+});
 
 CLIENT.login(myfs.getToken());
