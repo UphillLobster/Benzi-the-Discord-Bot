b744ac53b38cc59a01aaa4875d3a53d5785c139c
armedturret <rjwhitenh@outlook.com>
2018-08-08 21:46:09 -0400

    added function to get all humble bundle offers in last 48 hours

---
+++ rssgrab.js
@@ -0,0 +1,52 @@
+//grabs rss info from websites and outputs it
+//note that specialized functions for rss should also be added here i.e. humble bundle looks for the keyword free
+const myfs = require("./botfilesystem.js");
+const http = require("http");
+const parser = require('fast-xml-parser');
+const he = require('he');
+const request = require("request-promise-native");
+const moment = require("moment");
+
+//humble bundle rss page info
+const hbwebsite = 'http://blog.humblebundle.com/rss';
+
+//gets a webpage's contents
+exports.getWebpageContents = async function(uri){
+  var page = await request(uri);
+
+  return page;
+}
+
+
+const dif = 48;
+//gets all humble bundle deals in last dif hours (that's usually how long free games last)
+async function getHumbleBundleRss(){
+  //parse its xml
+  var humblebundlerss = parser.parse(await exports.getWebpageContents(hbwebsite));
+
+  var items = humblebundlerss.rss.channel.item;
+
+  var returnItems = [];
+
+  //get now
+  var cutoff = moment().subtract(dif,'hours');
+
+  for(var item in items){
+    //calculate if post was in last 48 hours
+    var timestring = items[item].pubDate;
+
+    //parse time
+    var date = moment(timestring, 'dddd, DD MMM YYYY HH:mm:ss Z','en');
+
+    //calculate if it's within dif hours
+    if(date.isAfter(cutoff))
+      returnItems.push(items[item]);
+  }
+  return returnItems;
+}
+
+async function test(){
+  console.log(await getHumbleBundleRss());
+}
+
+test();
